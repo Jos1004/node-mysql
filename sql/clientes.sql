1138053e9ce055d3defb80fd446153f0eb640418
Silva <86150617+Jos1004@users.noreply.github.com>
2025-01-24 15:42:03 -0300

    Add files via upload

---
+++ sql/clientes.sql
@@ -0,0 +1,21 @@
+/*Para criar o Database crud, usei a opção Create a new schema in the
+connect server, para quem não conhece é so clicar no simbolo cilindrico*/
+/*Em seguida abra o crud, botão direito em Tables e crie uma tabela chamada clientes*/
+/* E insira esse comandos após apertar Apply 
+id int auto_increment primary key,
+nome varchar(150),
+idade int,
+uf varchar(2)*/ 
+
+use crud;
+describe clientes;
+select * from clientes;
+
+insert into clientes (nome, idade, uf) values ('Hanna', 27, 'SP');
+insert into clientes (nome, idade, uf) values ('Carol', 28, 'BH');
+insert into clientes (nome, idade, uf) values ('Maria Clara', 18, 'MG');
+insert into clientes (nome, idade, uf) values ('Mariano', 34, 'SC');
+insert into clientes (nome, idade, uf) values ('Fellip', 22, 'PR');
+insert into clientes (nome, idade, uf) values ('Diego', 36, 'RJ');
+
+select*from clientes; 
